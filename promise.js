3f30ae5ad4128bd16ace0ac959d8b4d856917765
Godyakho <j.work@qq.com>
2018-06-16 21:39:17 +0800

    update

--- promise.js
+++ promise.js
@@ -25,13 +25,47 @@ function runAsync1(){
     return p;            
 }
 
-Promise.all([runAsync1()])
-.then(function(results){
-    console.log(results);
-});
+// Promise.all([runAsync1()])
+// .then(function(results){
+//     console.log(results);
+// });
 // catch   在执行resolve的回调（也就是上面then中的第一个参数）时，如果抛出异常了，那么并不会报错卡死js，而是会进到这个catch
 
-// Promise 实现
-function Promise() {
-
+// Promise 模仿实现
+function vPromise() {
+    this.callbacks = []
 }
+
+vPromise.prototype.then = function(fn) {
+    this.callbacks.push(fn)
+    return this
+}
+
+vPromise.prototype.resolve = function(data) {
+    var fn = this.callbacks.shift()
+    fn(data)    
+}
+
+var promise2 = new vPromise()
+
+getCity()
+  .then(getWeather)
+  .then(getSuggestion)
+
+function getCity(){
+  setTimeout(function(){
+    promise2.resolve('Beijing')
+  }, 1000)
+  return promise2   
+}
+
+function getWeather(city){
+    setTimeout(function(){
+    promise2.resolve(city + ' 晴天')
+    }, 1000)
+  }
+  function getSuggestion(weather){
+    setTimeout(function(){
+      console.log(weather + ' 天气不错')
+    }, 1000)
+  }
