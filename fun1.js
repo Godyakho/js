f784b56b48406b807dc91a463fa9527670db4685
Godyakho <j.work@qq.com>
2018-06-29 23:12:49 +0800

    update

--- fun1.js
+++ fun1.js
@@ -1,3 +1,4 @@
+// 数组先扁平化，在去重，后排序
 var arr = [[1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14]]]], 10];  
 
 var newarr = []
@@ -39,3 +40,16 @@ tar.sort(function(a, b){
 })
 
 console.log(tar)
+
+// url 数组获取
+var url = 'https://www.baidu.com/s?wd=js%20Array%20%E6%96%B9%E6%B3%95&rsv_spt=1&rsv_iqid=0x9e3ba5200001fa7a&issp=1&f=8&rsv_bp=0&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&rsv_sug3=30&rsv_sug1=22&rsv_sug7=101&rsv_t=0fa1lYsHWZo5omyxL2dZIcUr4wF2bJeJ1IkgiFuuIbsOowe%2FVw5mFy8pBeL24X6xJYma&rsv_sug2=0&inputT=17926&rsv_sug4=18167'
+var newarr = url.split('?')[1].split('&')
+var tem = []
+newarr.forEach((item) => {
+    var obj = {}
+    var key = item.split('=')[0]
+    var value = item.split('=')[1]
+    obj[key] = value
+    tem.push(obj)
+})
+console.log(JSON.stringify(tem))
